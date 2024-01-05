32003c4e62543a8d29e02742c2362403844b391b
MakMoinee <makmoinee@gmail.com>
2024-01-06 02:27:48 +0800

    update code

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,18 +22,57 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        btnStartService = New Button()
+        Label1 = New Label()
+        btnLogin = New Button()
         SuspendLayout()
+        ' 
+        ' btnStartService
+        ' 
+        btnStartService.Location = New Point(55, 135)
+        btnStartService.Name = "btnStartService"
+        btnStartService.Size = New Size(314, 94)
+        btnStartService.TabIndex = 0
+        btnStartService.Text = "Start Service"
+        btnStartService.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(200, 40)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(336, 50)
+        Label1.TabIndex = 1
+        Label1.Text = "Elder Watch Admin"
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.Location = New Point(388, 135)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(314, 94)
+        btnLogin.TabIndex = 2
+        btnLogin.Text = "Login"
+        btnLogin.UseVisualStyleBackColor = True
         ' 
         ' frmMain
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(611, 321)
+        ClientSize = New Size(770, 291)
+        Controls.Add(btnLogin)
+        Controls.Add(Label1)
+        Controls.Add(btnStartService)
         MaximizeBox = False
         Name = "frmMain"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents btnStartService As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnLogin As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,58 @@
-﻿Public Class frmMain
+﻿
+Imports System.Diagnostics
+Public Class frmMain
+    Private batchProcess As Process
+    Private Sub btnStartService_Click(sender As Object, e As EventArgs) Handles btnStartService.Click
+        Try
+            Dim batchProcessName As String = "start.bat"
 
+            ' Check if the batch file process is already running
+            If IsProcessRunning(batchProcessName) Then
+                MessageBox.Show("The process is already running.")
+                ' Disable the Start Service button
+                btnStartService.Enabled = False
+            Else
+                Dim batchFilePath As String = "C:\Users\Brian\Documents\elderWatchAI\"
+
+                batchProcess = New Process()
+                batchProcess.StartInfo.FileName = "cmd.exe"
+                batchProcess.StartInfo.Arguments = "/k cd /d C:\Users\Brian\Documents\elderWatchAI && " & batchFilePath & batchProcessName & ""
+                batchProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal
+                batchProcess.Start()
+
+                ' process.WaitForExit()
+
+                ' Enable the Start Service button after the process finishes
+                btnStartService.Enabled = True
+            End If
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckIfBatchFileIsRunning()
+    End Sub
+
+    Private Sub CheckIfBatchFileIsRunning()
+        Dim batchProcessName As String = "start.bat"
+        If IsProcessRunning(batchProcessName) Then
+            MessageBox.Show("The process is already running.")
+            ' Disable the Start Service button
+            btnStartService.Enabled = False
+        Else
+            ' Enable the Start Service button
+            btnStartService.Enabled = True
+        End If
+    End Sub
+
+    Private Function IsProcessRunning(processName As String) As Boolean
+        Dim processes() As Process = Process.GetProcessesByName(processName)
+        Return processes.Length > 0
+    End Function
+
+    Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        batchProcess.Kill()
+
+    End Sub
 End Class
